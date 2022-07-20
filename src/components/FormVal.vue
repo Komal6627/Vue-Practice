f8901eccb74a326bf7c1a70ad5450e12195ac0b0
Komal6627 <komalkilledar1996@gmail.com>
2022-07-20 17:46:56 +0530

    Done with validation

--- src/components/FormVal.vue
+++ src/components/FormVal.vue
@@ -1,16 +1,48 @@
 <template>
     <div>
         <h1>Form Validation</h1> 
-        <input type="text"  name="username" placeholder="Enter the User name" />
-        <br /><br />
-        <input type="password" name="password" placeholder="Enter the Password" />
-        <br /><br />
-        <button >Login</button>
-        </div>
+       <p v-if="error.length">
+          <b>Please correct the following error </b>
+          <ul>
+              <li v-for="e in error" v-bind:key="e.id">{{e}}</li>
+          </ul>
+       </p>
+        <form @submit="login">
+              <input type="text"  name="username" placeholder="Enter the User name" v-model="name"/>
+               <br /><br />
+              <input type="password" name="password" placeholder="Enter the Password" v-model="password"/>
+              <br /><br />
+              <button type="submit">Login</button>
+        </form>
+    </div>
+        
 </template>
 
 <script>
     export default {
-      name:"FormVal"
+      name:"FormVal",
+      data(){
+        return{
+          error:[],
+          name:null,
+          password:null
+        }
+      },
+      methods:{
+          login(e){
+            this.error=[];
+              if(this.name && this.password){
+                console.warn("no error");
+              }
+              if(!this.name){
+                this.error.push("User name require");
+              }
+              if(!this.password){
+                this.error.push("Password require");
+              }
+              console.warn("Hello",this.error);
+              e.preventDefault();
+          }
+      }
     }
 </script>
