13db46e8f73dc4798cebce79b181f0c5e415d58a
Komal6627 <komalkilledar1996@gmail.com>
2022-07-20 12:09:53 +0530

    1 file modified

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,21 +1,24 @@
 <template>
     <div>
           <h1>Form with checkbox and radio button </h1>
-        <input type="text" placeholder="Enter the name"/> <br><br>
-        <input type="password" placeholder="Enter the password"/> <br>
+        <input type="text" placeholder="Enter the name" v-model="signup.username"/> <br><br>
+        <input type="password" placeholder="Enter the password" v-model="signup.password" /> <br>
 
-        <p>Choose Hoobies</p>
-        <input type="checkbox" id="cricket" value="cricket"/>
+        <p >Choose Hoobies</p>
+        <input type="checkbox" id="cricket" value="cricket" v-model="signup.hobbies"/>
         <label for="cricket">Cricket</label>
-        <input type="checkbox" id="football" value="football"/>
+        <input type="checkbox" id="football" value="football" v-model="signup.hobbies"/>
         <label for="football">Badminton</label>
         <br>
 
-        <p>Select Gender</p>
-        <input type="radio" id="male" value="male"/>
+        <p >Select Gender</p>
+        <input type="radio" id="male" value="male" v-model="signup.gender"/>
         <label for="male">Male</label>
-        <input type="radio" id="male" value="male"/>
-        <label for="male">Female</label>
+        <input type="radio" id="female" value="female" v-model="signup.gender" />
+        <label for="female">Female</label>
+
+        <br>
+        <button v-on:click="signupUser">Sign Up</button>
     </div> 
 </template>
 
@@ -30,6 +33,11 @@
             hobbies:[],
             gender:null
           }
+        }
+      },
+      methods:{
+        signupUser(){
+          console.log(this.signup);
         }
       }
     }
