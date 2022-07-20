f3c4595242d454e7b443bac9acbeeea6855d7877
Komal6627 <komalkilledar1996@gmail.com>
2022-07-20 12:30:38 +0530

    1 file modified

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h1>Form</h1>
-        <input type="text" placeholder="Enter the user name"/>
+        <input type="text" name="username" placeholder="Enter the User name"/>
+        <br><br>
+        <input type="password" name="password" placeholder="Enter the Password"/>
        
     </div>  
 </template>
