5ff1189ac5862577d33757dae6b28f57c810beb3
Komal6627 <komalkilledar1996@gmail.com>
2022-07-21 10:34:55 +0530

    Filter component is pending

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 const { createApp } = require('vue');
 import App from "./App.vue";
 
-
-Vue.config.productionTip = false
-// Vue.filter("Ucase", function(val){
-//     return val.toUpperCase();
+// Vue.filter("Ucase",function(val){
+//    return val.toUpperCase();
 // })
+
 createApp(App).mount("#app");
